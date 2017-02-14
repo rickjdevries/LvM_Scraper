c7fac03062564989731b6f5a6057e4c84d739fe1
svanlochem <svanlochem@gmail.com>
2017-02-14 20:15:19 +0100

    Try-catch ingebouwd

--- activityScraper.py
+++ activityScraper.py
@@ -1,13 +1,13 @@
 from bs4      import BeautifulSoup
 from datetime import datetime
-import requests
-import feedparser
-
-#TEST
+import requests, ssl, feedparser
 
 def ZiggoDomeLoader():
     import locale
-    locale.setlocale(locale.LC_ALL,'nl_NL.UTF-8')#Dutch
+    try: #Rick
+        locale.setlocale(locale.LC_ALL,'nl_NL.UTF-8')#Dutch
+    except: #Sander
+        locale.setlocale(locale.LC_ALL,'Dutch_Netherlands.1252')#Dutch
 
     URL = 'https://www.ziggodome.nl/agenda'
     container = []
@@ -32,34 +32,54 @@ def ZiggoDomeLoader():
                           'time',
                           url])
 
-    locale.setlocale(locale.LC_ALL,'en_US.UTF-8')#English US
+    try: #Rick
+        locale.setlocale(locale.LC_ALL,'en_US.UTF-8')#English US
+    except: #Sander
+        locale.setlocale(locale.LC_ALL,'English_United States.1252')#English US
     return container
    
-def RSSloader(URL,VENUE):
+def Paardloader(URL):
     feed = feedparser.parse(URL)
     container = []
     #Loop over the events
-    if VENUE == 'Paradiso':
-        import locale
+    for event in feed['entries']:
+        container.append([event['title'].split(' ')[-1],
+                         datetime.strptime(event['date'],'%a, %d %b %Y %H:%M:%S %z').date(),
+                         datetime.strptime(event['time'],'%H:%M').time(),
+                         event['link']])
+    return container
+    
+def ParadisoLoader(URL):
+    if hasattr(ssl, '_create_unverified_context'):
+        ssl._create_default_https_context = ssl._create_unverified_context
+        
+    feed = feedparser.parse(URL)
+    container = []
+    #Loop over the events
+
+    import locale
+    try: #Rick
+        print('Rick')
         locale.setlocale(locale.LC_ALL,'nl_NL.UTF-8')#Dutch
-        
-        for event in feed['entries']:
-            info       = event['title'].split(' - ')
-            date_time  = datetime.strptime(info[0],'%A %d %B %Y %H:%M')
-        
-            container.append([info[1],date_time.date(),date_time.time(),event['link']])
+    except: #Sander
+        print('Sander')
+        locale.setlocale(locale.LC_ALL,'Dutch_Netherlands.1252')#Dutch
+
+    for event in feed['entries']:
+        info       = event['title'].split(' - ')
+        date_time  = datetime.strptime(info[0],'%A %d %B %Y %H:%M')
+    
+        container.append([info[1],date_time.date(),date_time.time(),event['link']])
+    
+    try: #Rick
         locale.setlocale(locale.LC_ALL,'en_US.UTF-8')#English US
-    elif VENUE == 'Paard':
-        for event in feed['entries']:
-            container.append([event['title'].split(' ')[-1],
-                             datetime.strptime(event['date'],'%a, %d %b %Y %H:%M:%S %z').date(),
-                             datetime.strptime(event['time'],'%H:%M').time(),
-                             event['link']])
+    except: #Sander
+        locale.setlocale(locale.LC_ALL,'English_United States.1252')#English US
     return container
 
 #Call functions to parse the RSS feeds
-paard_container     = RSSloader('http://www.paard.nl/programme/rss/lang/nl','Paard')
-paradiso_container  = RSSloader('https://www.paradiso.nl/rss.xml','Paradiso')
+paard_container     = Paardloader('http://www.paard.nl/programme/rss/lang/nl')
+paradiso_container  = ParadisoLoader('https://www.paradiso.nl/rss.xml')
 ziggodome_container = ZiggoDomeLoader()
 
 print(len(paard_container),len(paradiso_container),len(ziggodome_container))
