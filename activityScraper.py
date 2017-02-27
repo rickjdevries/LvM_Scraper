589b7b997a9a0b715b44267377e2899cd92494fd
Rick <rickjdevries@gmail.com>
2017-02-27 22:10:42 +0100

    print statements toegevoegd

--- activityScraper.py
+++ activityScraper.py
@@ -4,8 +4,12 @@ from Venues.ZiggoDome import ZiggoDomeLoader
 
 #Call functions to parse the RSS feeds
 paard_container     = PaardLoader('http://www.paard.nl/programme/rss/lang/nl')
-paradiso_container  = ParadisoLoader('https://www.paradiso.nl/rss.xml')
-ziggodome_container = ZiggoDomeLoader()
+#paradiso_container  = ParadisoLoader('https://www.paradiso.nl/rss.xml')
+#ziggodome_container = ZiggoDomeLoader()
 
 #print numbers
-print(len(paard_container),len(paradiso_container),len(ziggodome_container))
+#print("Paard: %s, Paradiso: %s, ZiggoDome: %s" % (len(paard_container),  len(paradiso_container), len(ziggodome_container)))
+
+print(paard_container)
+
+print ', '.join(paard_container)
