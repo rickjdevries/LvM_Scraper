5c60b2bd4f5e4eadc51a61ac13e92dfc169e3b00
Rick <rickjdevries@gmail.com>
2017-03-01 18:49:11 +0100

    Poging om 013 te scrapen

--- Venues/Tilburg013.py
+++ Venues/Tilburg013.py
@@ -12,29 +12,28 @@ def Tilburg013Loader():
     container = []
    
     #Scrape the main site for links to events
-    for link in BeautifulSoup(requests.get(URL).content,"html.parser").find('div',attrs={'class':'event-overview '}).findAll('a',href=True):
+    for link in BeautifulSoup(requests.get(URL).content,"html.parser").find('div',attrs={'class':'event-overview'}).findAll('a',href=True):
         #Abstract some information
-        event_date = link.find('span',attrs={'class':'event_date'}).text
-        event_act = link.find('span',attrs={'class':'event_act'}).text
+        #event_date = link.find('span',attrs={'class':'event_date'}).text
+        #event_act = link.find('span',attrs={'class':'event_act'}).text
 
-        #url = link['href']
-        ##open the event page
-        #event_data = BeautifulSoup(requests.get(url).content,"html.parser").find('div',attrs={'class':'titlebar'})
+        url = link['href']
+        #combine the hyperlinks
+        url = 'http://www.013.nl' + url
+        #print(url)
         
-        #date  = event_data.find('h2',attrs={'class':'event_date'}).text
+        ##open the event page
+        event_data = BeautifulSoup(requests.get(url).content,"html.parser").find('div',attrs={'class':'spotlight'})
+        
+        title = event_data.find('h1',attrs={'class':'spotlight__title title--alpha'}).text
+        date  = event_data.find('span',attrs={'class':'spotlight__date'}).text
         #date_time  = datetime.strptime(date.strip(),'%A %d %B %Y')
         
-        ##TODO: Abstract starting time from event page
-
-        #container.append([event_data.find('h1',attrs={'class':'event_title'}).text,
-                          #date_time.date(),
-                          #'time',
-                          #url])
+        container.append([title, date])          
                           
-    container = event_act
-
     try: #Rick
         locale.setlocale(locale.LC_ALL,'en_US.UTF-8')#English US
     except: #Sander
         locale.setlocale(locale.LC_ALL,'English_United States.1252')#English US
+        
     return container
--- activityScraper.py
+++ activityScraper.py
@@ -1,7 +1,7 @@
 from Venues.Paradiso  	import ParadisoLoader
 from Venues.Paard    	import PaardLoader
 from Venues.ZiggoDome 	import ZiggoDomeLoader
-from Venues.AFASLive  	import AFASLiveLoader
+#from Venues.AFASLive  	import AFASLiveLoader
 from Venues.Boerderij   import BoerderijLoader
 from Venues.Tilburg013	import Tilburg013Loader
 
@@ -16,13 +16,13 @@ from Venues.Tilburg013	import Tilburg013Loader
 #print(paard_container)
 
 #print ', '.join(paard_container)
-#Boerderij_container = BoerderijLoader()
-#AFAS_container      = AFASLiveLoader()
-#paard_container     = PaardLoader()
-#paradiso_container  = ParadisoLoader()
-#ziggodome_container = ZiggoDomeLoader()
-Tilburg013_container       = Tilburg013Loader()
+Boerderij_container  = BoerderijLoader()
+#AFAS_container       = AFASLiveLoader()
+paard_container      = PaardLoader()
+paradiso_container   = ParadisoLoader()
+ziggodome_container  = ZiggoDomeLoader()
+Tilburg013_container = Tilburg013Loader()
 
 #print numbers
-#print(len(Boerderij_container),len(AFAS_container),len(paard_container),len(paradiso_container),len(ziggodome_container))
-print(len(Tilburg013_container))
+print("Boerderij: %s, Paard: %s, Paradiso: %s, ZiggoDome: %s, 013: %s" % (len(Boerderij_container),len(paard_container), len(paradiso_container), len(ziggodome_container), len(Tilburg013_container)))
+print(len(Boerderij_container),len(paard_container),len(paradiso_container),len(ziggodome_container),len(Tilburg013_container))
