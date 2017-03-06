e78b26fea9d8580de41b03492eafdb0fa4207184
svanlochem <svanlochem@gmail.com>
2017-03-06 22:02:38 +0100

    Luxor Live toegevoegd

---
+++ Venues/LuxorLive.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from bs4      import BeautifulSoup
+import locale, requests
+
+def LuxorLiveLoader():
+    try: #Rick
+        locale.setlocale(locale.LC_ALL,'nl_NL.UTF-8')#Dutch
+    except: #Sander
+        locale.setlocale(locale.LC_ALL,'Dutch_Netherlands.1252')#Dutch
+
+    URL = 'https://www.luxorlive.nl/agenda/'
+    container = []
+   
+    #Scrape the main site for links to events
+    for link in BeautifulSoup(requests.get(URL).content,"html.parser").findAll('div',attrs={'class':'item horizontal '}):
+        title    = link.find('h3').text
+        url      = link.find('a')['href']
+        raw_date = link.find('meta',attrs={'itemprop':'startDate'})['content'].split('+')
+        
+        date = datetime.strptime(raw_date[0] + '+' + raw_date[-1].replace(':',''),'%Y-%m-%dT%H:%M:%S%z')
+        time = None
+
+        for entry in link.find('ul').findAll('li'):
+            if 'start' in str(entry):
+                time = datetime.strptime(str(entry).split('start ',1)[1][:5],'%H:%M').time()
+        
+        container.append([title,
+                          date,
+                          time,
+                          url])
+        
+    try: #Rick
+        locale.setlocale(locale.LC_ALL,'en_US.UTF-8')#English US
+    except: #Sander
+        locale.setlocale(locale.LC_ALL,'English_United States.1252')#English US
+    return container
+        
+LuxorLiveLoader()
--- activityScraper.py
+++ activityScraper.py
@@ -6,6 +6,7 @@ from Venues.Boerderij   import BoerderijLoader
 from Venues.Tilburg013	import Tilburg013Loader
 from Venues.Melkweg 	import MelkwegLoader
 from Venues.Arena       import ArenaLoader
+from Venues.LuxorLive   import LuxorLiveLoader
 
 #Call functions to parse the RSS feeds
 Boerderij_container  = BoerderijLoader()
@@ -16,6 +17,7 @@ ziggodome_container  = ZiggoDomeLoader()
 Tilburg013_container = Tilburg013Loader()
 melkweg_container    = MelkwegLoader()
 arena_container      = ArenaLoader()
+luxorlive_container  = LuxorLiveLoader()
 
 #print numbers
-print("Boerderij: %s, Paard: %s, Paradiso: %s, ZiggoDome: %s, 013: %s, Melkweg: %s, Arena: %s" % (len(Boerderij_container),len(paard_container), len(paradiso_container), len(ziggodome_container), len(Tilburg013_container), len(melkweg_container), len(arena_container)))
+print("Boerderij: %s, Paard: %s, Paradiso: %s, ZiggoDome: %s, 013: %s, Melkweg: %s, Arena: %s, LuxorLive: %s" % (len(Boerderij_container),len(paard_container), len(paradiso_container), len(ziggodome_container), len(Tilburg013_container), len(melkweg_container), len(arena_container), len(luxorlive_container)))
