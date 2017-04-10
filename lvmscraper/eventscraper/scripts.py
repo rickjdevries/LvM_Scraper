1eaa6ea8e2b9ef4a81027a226652f2018b6946b3
svanlochem <svanlochem@gmail.com>
2017-04-10 20:48:15 +0200

    Pandas uit code gehaald tbv server deployment apache2

--- lvmscraper/eventscraper/scripts.py
+++ lvmscraper/eventscraper/scripts.py
@@ -1,30 +1,7 @@
 from .models               import Event
 import eventscraper.Venues as     venues
-import pandas              as     pd
 import math
-
-def import_file(f):   
-    #Import the file with pandas
-    df = pd.read_csv(f, encoding = "ISO-8859-1")
-
-    #loop over the rows and store them in an object
-    for index, row in df.iterrows():
-        if type(row.Time) is str:
-            Event.objects.get_or_create(
-                venue = row.Venue,
-                title = row.Title,
-                date  = row.Date,
-                time  = row.Time,
-                URL   = row.URL
-            )
-        else:
-            Event.objects.get_or_create(
-                venue = row.Venue,
-                title = row.Title,
-                date  = row.Date,
-                URL   = row.URL
-        )
-        
+       
 def scrape_events():
     #Call functions to parse the RSS feeds
     Boerderij_container  = venues.BoerderijLoader()
--- lvmscraper/eventscraper/urls.py
+++ lvmscraper/eventscraper/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = 'event_scraper'
 urlpatterns = [
-    # url(r'^$', views.Index.as_view(), name='index'),
     url(r'^event_list/$', views.EventListView.as_view(), name='event_list'),
     url(r'^event_list/(?P<venue>[\w ]+)/$', views.VenueView.as_view(), name='venue_list'),
     url(r'^venue_list/$', views.VenueListView.as_view(), name='venue_list_view'),
